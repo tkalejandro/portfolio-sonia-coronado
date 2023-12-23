cc869d78ba1a3769f785f16ef89111919ac7cb80
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2023-12-23 19:08:50 +0100

    solved deleted testexperience

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import TestExperience from '@/modules/Experience/TestExperience';
+import React from 'react';
+
+/**
+ * Test Page for experience
+ * @returns
+ */
+export default function Test() {
+  return <TestExperience />;
+}
